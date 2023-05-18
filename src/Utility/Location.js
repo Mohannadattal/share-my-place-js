6bc531d585edb4fd5fea81a8c868f58c4a5ec497
Mohannadgithub <mohannadattal85@gmail.com>
2023-05-18 15:26:51 +0200

    Converting User Input to Coordinates

--- src/Utility/Location.js
+++ src/Utility/Location.js
@@ -1,22 +1,34 @@
-const BING_API_KEY =
-  'Au5Ue79FIOd-G1nbMQIQEjeT6h_VUteohg4nrHLIv2n1ZGXFdM9tINR5ct_jUclk';
+const BING_API_KEY = 'Au5Ue79FIOd-G1nbMQIQEjeT6h_VUteohg4nrHLIv2n1ZGXFdM9tINR5ct_jUclk';
 
 export async function getCoordsFromAddress(address) {
   const urlAddress = encodeURI(address);
   const response = await fetch(
-    `http://dev.virtualearth.net/REST/v1/Locations?address=${urlAddress}&key=${BING_API_KEY}`
+    `https://dev.virtualearth.net/REST/v1/Locations?addressLine=${urlAddress}&key=${BING_API_KEY}`
   );
 
   if (!response.ok) {
-    throw new Error('Failed to fetch coordinates. Please try again');
+    throw new Error('Failed to fetch coordinates. Please try again!');
   }
 
   const data = await response.json();
 
-  if (data.error_message) {
-    throw new Error(data.error_message);
+  if (data.errorDetails) {
+    throw new Error(data.errorDetails[0]);
   }
 
-  const coordinates = data.results[0].geometry.location;
-  return coordinates;
+  const resourceSets = data.resourceSets;
+  if (resourceSets.length === 0) {
+    throw new Error('Coordinates not found.');
+  }
+
+  const resources = resourceSets[0].resources;
+  if (resources.length === 0) {
+    throw new Error('Coordinates not found.');
+  }
+
+  const coordinates = resources[0].point.coordinates;
+  return {
+    latitude: coordinates[0],
+    longitude: coordinates[1]
+  };
 }
