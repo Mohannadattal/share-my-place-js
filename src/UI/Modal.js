44ae2d0682e3fc6b9eedc737a1333145d1883088
Mohannadgithub <mohannadattal85@gmail.com>
2023-05-18 10:38:22 +0200

    Hiding the Modal

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -1,32 +1,39 @@
 export class Modal {
-    constructor(contentId, fallbackText) {
-      this.fallbackText = fallbackText;
-      this.contentTemplateEl = document.getElementById(contentId);
-      this.modalTemplateEl = document.getElementById('modal-template');
-    }
-  
-    show() {
-      if ('content' in document.createElement('template')) {
-        const modalElements = document.importNode(
-          this.modalTemplateEl.content,
-          true
-        );
-        const modalElement = modalElements.querySelector('.modal');
-        const backdropElement = modalElements.querySelector('.backdrop');
-        const contentElement = document.importNode(
-          this.contentTemplateEl.content,
-          true
-        );
-  
-        modalElement.appendChild(contentElement);
-  
-        document.body.insertAdjacentElement('afterbegin', modalElement);
-        document.body.insertAdjacentElement('afterbegin', backdropElement);
-      } else {
-        // fallback code
-        alert(this.fallbackText);
-      }
-    }
-  
-    hide() {}
+  constructor(contentId, fallbackText) {
+    this.fallbackText = fallbackText;
+    this.contentTemplateEl = document.getElementById(contentId);
+    this.modalTemplateEl = document.getElementById('modal-template');
   }
+
+  show() {
+    if ('content' in document.createElement('template')) {
+      const modalElements = document.importNode(
+        this.modalTemplateEl.content,
+        true
+      );
+      this.modalElement = modalElements.querySelector('.modal');
+      this.backdropElement = modalElements.querySelector('.backdrop');
+      const contentElement = document.importNode(
+        this.contentTemplateEl.content,
+        true
+      );
+
+      this.modalElement.appendChild(contentElement);
+
+      document.body.insertAdjacentElement('afterbegin', this.modalElement);
+      document.body.insertAdjacentElement('afterbegin', this.backdropElement);
+    } else {
+      // fallback code
+      alert(this.fallbackText);
+    }
+  }
+
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement);
+      document.body.removeChild(this.backdropElement);
+      this.modalElement = null;
+      this.backdropElement = null;
+    }
+  }
+}
